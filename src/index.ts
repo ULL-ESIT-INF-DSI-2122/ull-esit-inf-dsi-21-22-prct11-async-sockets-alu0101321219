d19429980cfd9351b2d4b075ebe2ff03d27c215d
Adrián González Galván <alu0101321219@ull.edu.es>
2022-04-19 23:18:28 +0000

    Empezamos a testear con yargs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as yargs from 'yargs';
+import * as fs from 'fs';
+import {Note} from './note';
 
 yargs.command({
   command: 'add',
@@ -26,8 +28,23 @@ yargs.command({
     },
   },
   handler(argv) {
-    if (typeof argv.title === 'string') {
-      // Required logic to add a new note
+    if (typeof argv.user === 'string' && typeof argv.title === 'string' &&
+      typeof argv.body === 'string' && typeof argv.color === 'string') {
+      const myNote: Note = new Note(argv.title, argv.body, 'red');
+      fs.mkdir(`./notes/${argv.user}`, (err) => {
+        if (err) {
+          console.log("Somenthing went wrong when writing your folder");
+        } else {
+          console.log("Folder has just been created");
+        }
+      });
+      fs.writeFile(`./notes/${argv.user}/${argv.title}.json`, JSON.stringify(myNote), (err) => {
+        if (err) {
+          console.log('Something went wrong when writing your file');
+        } else {
+          console.log('File helloworld.txt has just been created');
+        }
+      });
     }
   },
 });
