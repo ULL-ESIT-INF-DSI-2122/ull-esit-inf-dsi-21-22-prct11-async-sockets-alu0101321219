3862d6d564c818967a946d689a1429bee23a4c9f
Adrián González Galván <alu0101321219@ull.edu.es>
2022-04-19 22:34:26 +0000

    Incluimos clase Note

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
-/**
- * Suma 2 numeros
- * @param n1 numero 1
- * @param n2 numero 2
- * @returns suma
- */
-export function add(n1: number, n2: number): number {
-  return n1 + n2;
-}
+import * as yargs from 'yargs';
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    user: {
+      describe: 'Note owner',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+    color: {
+      describe: 'Note color',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.title === 'string') {
+      // Required logic to add a new note
+    }
+  },
+});
+
+yargs.parse();
