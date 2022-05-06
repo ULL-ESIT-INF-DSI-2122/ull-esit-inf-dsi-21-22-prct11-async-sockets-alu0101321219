b8bc2dffe32ef3af053cb2965dc4912990f47951
Adrián González Galván <alu0101321219@ull.edu.es>
2022-05-06 20:54:27 +0000

    Comentamos clase evenEmitterClient

--- src/eventEmitterClient.ts
+++ src/eventEmitterClient.ts
@@ -1,6 +1,14 @@
 import {EventEmitter} from "events";
 
+/**
+ * Clase que actúa como cliente, esta hereda de EventEmitter con el
+ * objetivo de procesar mensajes de respuesta de un servidor.
+ */
 export class EventEmitterClient extends EventEmitter {
+  /**
+   * Inicializa un objeto de la clase 'EvenEmitterClient'
+   * @param connection Objeto EventEmitter que establece la conexión
+   */
   constructor(connection: EventEmitter) {
     super();
     let wholeData = '';
